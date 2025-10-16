d58cb2e092735324712c9d6b27076a43a30c386c
ZaviX <87973889+ZaviiiX@users.noreply.github.com>
2025-10-16 08:58:13 +0200

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,10 +1,10 @@
 // Environment configuration
 export const CONTRACT_ADDRESS = 
-  import.meta.env.VITE_CONTRACT_ADDRESS || "0xe628dcb3f0ce02a86bd1e3136ab8229293ca4444";
+  import.meta.env.VITE_CONTRACT_ADDRESS || "Fuv9hkphdYdtnMzNhdYSjvTTY9tM6Vrckzmx4XcWEkjV";
 
 export const BUY_URL = 
   import.meta.env.VITE_BUY_URL || 
-  "https://dexscreener.com/bsc/0xe628dcb3f0ce02a86bd1e3136ab8229293ca4444";
+  "https://dexscreener.com/bsc/Fuv9hkphdYdtnMzNhdYSjvTTY9tM6Vrckzmx4XcWEkjV";
 
 // Roadmap configuration
 export const ROADMAP_CONFIG = {
